4acd2fee9d4eacdf12f172c1d329a316d1c7f0a2
wenderson guedes <wendersonguedes6@gmail.com>
2024-09-22 23:44:31 -0300

    refactor: utlizando lambda functions e filtrando dados

--- the-fundamentals/index.php
+++ the-fundamentals/index.php
@@ -7,51 +7,73 @@
 </head>
 
 <body>
-    <?php
-    $books = [
-        [
-            'name' => 'Do Androids Dream of Electric Sheep',
-            'author' => 'Philip K. Dick',
-            'releaseYear' => 1968,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'Project Hail Mary',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2021,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'The Martian',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2011,
-            'purchaseUrl' => 'http://example.com'
-        ],
-    ];
+<?php
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ],
+];
 
-    function filterByAuthor($books, $author)
-    {
-        $filteredBooks = [];
+/**
+ * Filters an array of items using a given callback function.
+ *
+ * This function iterates through each item in the input array
+ * and applies the provided callback function to determine if
+ * the item should be included in the filtered result.
+ *
+ * @param array $items Items to filter.
+ * @param callable $fn Callback function that receives each item as a parameter.
+ *                     It should return true to keep the item, or false to exclude it.
+ *
+ * @return array Filtered items that satisfy the condition set by the callback.
+ */
+function filter($items, $fn)
+{
+    $filteredItems = [];
 
-        foreach ($books as $book) {
-            if ($book['author'] === $author) {
-                $filteredBooks[] = $book;
-            }
+    foreach ($items as $item) {
+        if ($fn($item)) {
+            $filteredItems[] = $item;
         }
-
-        return $filteredBooks;
     }
-    ?>
 
-    <ul>
-        <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
-            <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    return $filteredItems;
+}
+
+/**
+ * Filtering by release year biggest or equal 2000.
+ */
+$filteredBooks = filter($books, function ($book) {
+    return $book['releaseYear'] < 2000;
+});
+
+?>
+
+
+<ul>
+    <?php foreach ($filteredBooks as $book) : ?>
+        <li>
+            <a href="<?= $book['purchaseUrl'] ?>">
+                <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
 </body>
 
 </html>
