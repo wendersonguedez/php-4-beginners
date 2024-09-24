470c43cd69018a7d4af81eaccfeb8ccb04033f37
wenderson guedes <wendersonguedes6@gmail.com>
2024-09-24 13:57:09 -0300

    feat: adicionado método para debugar

--- helpers.php
+++ helpers.php
@@ -4,3 +4,11 @@ function urlContains(string $url): bool
 {
     return str_contains($_SERVER['REQUEST_URI'], $url);
 }
+
+function dd($content)
+{
+    echo '<pre>';
+    var_dump($content);
+    echo '</pre>';
+    die;
+}
