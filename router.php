3067b4d5de0f2b267f0e680588ecdc2e69d343b2
wenderson guedes <wendersonguedes6@gmail.com>
2024-09-24 23:00:19 -0300

    feat: criado arquivo e implementada lógica para roteamento básico

---
+++ router.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * => /
+ * => /about
+ * => /contact 
+ */
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+routeToController($uri, $routes);
+
+function abort($status_code = 404)
+{
+    http_response_code($status_code);
+
+    require "views/{$status_code}.view.php";
+    die();
+}
+
+function routeToController($uri, $routes)
+{
+    if (!array_key_exists($uri, $routes)) {
+        abort();
+    }
+    require $routes[$uri];
+}
