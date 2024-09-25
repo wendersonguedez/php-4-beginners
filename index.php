a8a4e2305d4cc7a0b8fae2dda216c32c997ee80a
wenderson guedes <wendersonguedes6@gmail.com>
2024-09-24 23:18:25 -0300

    refactor: extraindo código para outros arquivos, separando responsabilidades

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
 
 require 'helpers.php';
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-}
+require 'router.php';
