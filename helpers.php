6dc566eedcaba2a8ab93774da0ae9848e7cd1f39
wenderson guedes <wendersonguedes6@gmail.com>
2024-09-24 22:58:51 -0300

    refactor: melhorada lógica do método que verifica a url atual

--- helpers.php
+++ helpers.php
@@ -1,14 +1,29 @@
 <?php
 
-function urlContains(string $url): bool
+/**
+ * Check if the current URL matches the given requested URL.
+ *
+ * @param string $requestedUrl The URL to check against.
+ *
+ * @return bool True if the current URL matches the given requested URL, false otherwise.
+ */
+function urlIs(string $requestedUrl): bool
 {
-    return str_contains($_SERVER['REQUEST_URI'], $url);
+    return $_SERVER['REQUEST_URI'] === $requestedUrl;
 }
 
+/**
+ * Dump the given content and die.
+ *
+ * A convenient way to quickly debug variables. The given content is
+ * dumped to the screen and then the script execution is halted.
+ *
+ * @param mixed $content Content to dump to the screen.
+ */
 function dd($content)
 {
     echo '<pre>';
     var_dump($content);
     echo '</pre>';
-    die;
+    die();
 }
