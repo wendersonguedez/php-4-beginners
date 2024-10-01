52c3771e8f1295b73376c0bc93550e61a3d530e9
wenderson guedes <wendersonguedes6@gmail.com>
2024-10-01 00:24:39 -0300

    feat: utilizando bind param para evitar sql injection

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
 require 'helpers.php';
-require 'router.php';
+// require 'router.php';
+require 'Database.php';
+
+$config = require 'config.php';
+$database = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
+
+$posts = $database->query($query, [':id' => $id])->fetchAll();
