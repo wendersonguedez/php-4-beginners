92f5a3d3c6f22e8170b2057b2908735327cfa09e
wenderson guedes <wendersonguedes6@gmail.com>
2024-10-01 00:23:46 -0300

    feat: criado arquivo de conexão com banco de dados, usando pdo

---
+++ Database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database
+{
+    public PDO $connection;
+
+    public function __construct(array $config, $username = 'root', $password = '')
+    {
+        // mysql:host=localhost;port=3306;dbname=php_4_beginners
+        $dsn = "mysql:" . http_build_query($config, '', ';');
+
+        $password = 'caraidigdin';
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = [])
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute($params);
+
+        return $statement;
+    }
+}
