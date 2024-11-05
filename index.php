953cee1216c8e24813dca33d3cbba576e8d6d5f4
wenderson <wendersonguedes6@gmail.com>
2024-11-05 13:42:37 -0300

    chore: mudança de posição do arquivo de rotas

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
 require 'helpers.php';
-// require 'router.php';
 require 'Database.php';
+require 'router.php';
 
 $config = require 'config.php';
 $database = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "SELECT * FROM posts WHERE id = :id";
-
-$posts = $database->query($query, [':id' => $id])->fetchAll();
