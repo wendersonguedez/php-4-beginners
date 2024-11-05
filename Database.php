475948342235ab628dbf2d8c0cdabbe534f2d685
wenderson <wendersonguedes6@gmail.com>
2024-11-05 13:32:25 -0300

    refactor: adicionando comentarios as funções

--- Database.php
+++ Database.php
@@ -4,17 +4,32 @@ class Database
 {
     public PDO $connection;
 
+    /**
+     * Construct a new Database object.
+     *
+     * @param array $config Database connection options.
+     * @param string $username Username to use for the connection.
+     * @param string $password Password to use for the connection.
+     */
     public function __construct(array $config, $username = 'root', $password = '')
     {
         // mysql:host=localhost;port=3306;dbname=php_4_beginners
         $dsn = "mysql:" . http_build_query($config, '', ';');
-
         $password = '';
+        
         $this->connection = new PDO($dsn, $username, $password, [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
     }
 
+    /**
+     * Prepare and execute a SQL query.
+     *
+     * @param string $query The SQL query to execute.
+     * @param array $params An array of parameters to pass to the query.
+     *
+     * @return PDOStatement The prepared statement.
+     */
     public function query($query, $params = [])
     {
         $statement = $this->connection->prepare($query);
